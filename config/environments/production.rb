13977f0b5b5b8aed1225012ff5146a4aff689d01
Ty Rauber <tyrauber@mac.com>
2012-10-20 15:27:26 -0700

    compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,9 @@ IVoted2012::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
 
+  config.assets.compile = true
+  
   # Generate digests for assets URLs
   config.assets.digest = true
 
