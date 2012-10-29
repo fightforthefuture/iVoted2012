12845952daafa02cb83670cacbd1dd99034776d6
Ty Rauber <tyrauber@mac.com>
2012-10-29 09:37:53 -0700

    Fix tweet url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::Base
   end
   
   def default_tweet
-    idz = current_user.twitter_screen_name
+    idz = current_provider.uuid
     status = ""
     status = "Check out my new twitter badge and my personal voter page http://www.ivoted2012.org/twitter/#{idz} #iwillvote" if current_user
     status = "I voted! And here's my voter page http://www.ivoted2012.org/twitter/#{idz} #ivoted" if current_user && current_user.voted?
