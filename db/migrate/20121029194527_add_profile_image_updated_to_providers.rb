b3df6be68de40d3cd5398163e8c7021b65b3690e
Ty Rauber <tyrauber@mac.com>
2012-10-30 04:53:27 -0700

    remove profile image migration

--- db/migrate/20121029194527_add_profile_image_updated_to_providers.rb
+++
@@ -1,8 +0,0 @@
-class AddProfileImageUpdatedToProviders < ActiveRecord::Migration
-  def up
-    add_column :providers, :profile_image_url, :string
-  end
-  def down
-    remove_column :providers, :profile_image_url
-  end
-end
